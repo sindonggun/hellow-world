b4936779743f2100077d36341a05c415b90c6d5a
zlskqk <zlskqk@gmail.com>
2017-08-16 13:30:52 +0900

    commit

--- src/testpack/HellowWorld.java
+++ src/testpack/HellowWorld.java
@@ -2,6 +2,7 @@ package testpack;
 
 public class HellowWorld {
 	public static void main(String[] args){
-		System.out.print("Hello World");
+		System.out.println("Hello World");
+		System.out.println("test");
 	}
 }
